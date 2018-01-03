ac82b8d64bf5339c6d0bb15ffe1ad8614ca5189e
Benoît Béranger <benoit.beranger@icm-institute.org>
2018-01-03 15:52:13 +0100

    update script_test_trigger.m : easier to read

--- script_test_trigger.m
+++ script_test_trigger.m
@@ -1,21 +1,36 @@
+%% Clean up
+
 close all
 clear all
 clc
 
-%%
+% Check if Psychtoolbox is installed
+assert( ~isempty(which('PsychtoolboxVersion')) , 'Psychtoolbox not detected' );
+
+%% Send pulses
 
 msg = 255;
 
 WaitSecs(0.500);
 
+pulse = 0;
+
 while ~KbCheck
+    
+    pulse = pulse + 1;
+    
+    fprintf('Sending pulse #%d \n',pulse)
     
     parport_dev(msg);
 %     parport_adr(msg);
+
     WaitSecs(0.001);
+    
     parport_dev(0);
 %     parport_adr(0);
     
     WaitSecs(0.500);
     
 end
+
+fprintf('End \n')
